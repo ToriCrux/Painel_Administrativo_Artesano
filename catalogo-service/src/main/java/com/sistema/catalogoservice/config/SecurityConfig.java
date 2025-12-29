810554f2c695e6ca7b39a0f9cbd6da0c45ca8878
Luiz Carlos <133159894+GameOfFront@users.noreply.github.com>
2025-12-29 13:49:50 -0300

    att PATCH cors

--- catalogo-service/src/main/java/com/sistema/catalogoservice/config/SecurityConfig.java
+++ catalogo-service/src/main/java/com/sistema/catalogoservice/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
-		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
 		configuration.setMaxAge(3600L);
